7dc6bfdc43b0f35506851ddabd9b4bc3a8b1a808
ellachamb <ellachamb@gmail.com>
2024-01-18 11:58:52 -0700

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random(); 
+        int magicNumber = randomGenerator.Next(1, 11); 
+
+        int guess = -1; 
+
+        do {
+        Console.Write("What is your guess? "); 
+        string userInput1 = Console.ReadLine(); 
+        guess = int.Parse(userInput1);
+
+        if (guess == magicNumber) 
+        {
+            Console.WriteLine("You guessed it!"); 
+        }
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher"); 
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower"); 
+        }
+        } while (guess != magicNumber); 
     }
 }
